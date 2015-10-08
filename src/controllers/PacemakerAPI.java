c0bf44ad23b47ff62df0870e9f79330c3590d34a
HP <HP@yahoo>
2015-10-08 17:50:42 +0700

    Changes for second lab

---
+++ src/controllers/PacemakerAPI.java
@@ -0,0 +1,40 @@
+package controllers;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.User;
+
+public class PacemakerAPI {
+	private Map<Long, User> userIndex = new HashMap<>();
+	private Map<String, User> emailIndex = new HashMap<>();
+
+	public Collection<User> getUsers() {
+		return userIndex.values();
+	}
+
+	public void deleteUsers() {
+		userIndex.clear();
+		emailIndex.clear();
+	}
+
+	public User createUser(String firstName, String lastName, String email, String password) {
+		User user = new User(firstName, lastName, email, password);
+		userIndex.put(user.id, user);
+		emailIndex.put(email, user);
+		return user;
+	}
+
+	public User getUserByEmail(String email) {
+		return emailIndex.get(email);
+	}
+
+	public User getUser(Long id) {
+		return userIndex.get(id);
+	}
+
+	public void deleteUser(Long id) {
+		User user = userIndex.remove(id);
+		emailIndex.remove(user.email);
+	}
+}
--- src/models/User.java
+++ src/models/User.java
@@ -1,21 +1,34 @@
 package models;
 
-public class User 
-{
-  public String firstName;
-  public String lastName;
-  public String email;
-  public String password;
+import static com.google.common.base.MoreObjects.toStringHelper;
+import com.google.common.base.Objects;
 
-  public User()
-  {
-  }
+public class User {
+	static Long counter = 0l;
+	public Long id;
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String password;
 
-  public User(String firstName, String lastName, String email, String password)
-  {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-  }
+	public User() {
+	}
+
+	public User(String firstName, String lastName, String email, String password) {
+		this.id = counter++;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String toString() {
+		return toStringHelper(this).addValue(firstName).addValue(lastName).addValue(password).addValue(email)
+				.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);
+	}
 }
