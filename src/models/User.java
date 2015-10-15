bfa9a9d44d0243829847dd3516dc5ecc33d4b6db
HP <HP@yahoo>
2015-10-15 17:57:12 +0700

    Add tests

--- src/models/User.java
+++ src/models/User.java
@@ -1,6 +1,10 @@
 package models;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.common.base.Objects;
 
 public class User {
@@ -10,7 +14,8 @@ public class User {
 	public String lastName;
 	public String email;
 	public String password;
-
+	public Map<Long, Activity> activities = new HashMap<>();
+	
 	public User() {
 	}
 
@@ -23,7 +28,7 @@ public class User {
 	}
 
 	public String toString() {
-		return toStringHelper(this).addValue(firstName).addValue(lastName).addValue(password).addValue(email)
+		return toStringHelper(this).addValue(id).addValue(firstName).addValue(lastName).addValue(password).addValue(email)
 				.toString();
 	}
 
